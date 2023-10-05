2d2641f5bcf3625dfe0d8f6b9071fc4496f42d54
jaysqvl <jaysqvl@gmail.com>
2023-10-05 04:23:33 -0700

    Added googletest submodule (still broken)

---
+++ tests/tools/test_chatserver.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "Server.h"
+#include "GeneralManager.hpp"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+TEST(ChatServerTest, Function1) {
+    // ASSERT_EQ(1, function1());  // Replace with actual tests
+}
