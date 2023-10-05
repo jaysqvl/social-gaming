e137aea38b41469c326543bde0393b1041551027
jaysqvl <jaysqvl@gmail.com>
2023-10-05 04:23:33 -0700

    Added googletest submodule (still broken)

---
+++ tests/lib/test_Element.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "Element.hpp"  // Your module header
+
+TEST(ElementTest, Function1) {
+    // ASSERT_EQ(1, function1());  // Replace with actual tests
+}
---
+++ tests/lib/test_GeneralManager.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "GeneralManager.hpp"  // Your module header
+
+TEST(GeneralManagerTest, Function1) {
+    // ASSERT_EQ(1, function1());  // Replace with actual tests
+}
