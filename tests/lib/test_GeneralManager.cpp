7205965419fa312c356906511eab7b93e0a6b966
bde4 <jay_esquivel@sfu.ca>
2023-10-16 04:43:58 -0700

    Finally understand tests, still figuring out mocks

--- tests/lib/test_GeneralManager.cpp
+++ tests/lib/test_GeneralManager.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
+
 #include "GeneralManager.hpp"  // Your module header
+
+using namespace testing;
 
 TEST(GeneralManagerTest, Function1) {
     // ASSERT_EQ(1, function1());  // Replace with actual tests
