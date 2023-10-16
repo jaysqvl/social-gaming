82e58059b0dfd077cdeb8fb2f7750b1d2f850e76
bde4 <jay_esquivel@sfu.ca>
2023-10-16 04:43:58 -0700

    Finally understand tests, still figuring out mocks

--- tests/lib/test_Element.cpp
+++ tests/lib/test_Element.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
+
 #include "Element.hpp"  // Your module header
+
+using namespace testing;
 
 TEST(ElementTest, Function1) {
     // ASSERT_EQ(1, function1());  // Replace with actual tests
