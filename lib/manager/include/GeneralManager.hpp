532e209b089605fc07565400e4c8d7c1b8a294b3
mjm26 <mjm26@sfu.ca>
2023-10-17 16:59:19 -0700

    added router packets

--- lib/manager/include/GeneralManager.hpp
+++ lib/manager/include/GeneralManager.hpp
@@ -17,10 +17,10 @@ public:
 
     void onConnect(Connection conn) override;
     void onDisconnect(Connection conn) override;
-    void processMessages(Server &server, std::deque<Message> &outgoing, const std::deque<Message>& incoming) override;
-    void buildOutgoing(std::deque<Message> &outgoing, const Message &message) override;
+    void processMessages(Server &server, std::deque<Packet> &outgoing, const std::deque<Message>& incoming) override;
+    void buildOutgoing(std::deque<Message> &outgoing, const Packet &packet) override;
     bool shouldQuit(void) override;
-    
+
 private:
     std::vector<Connection> clients;
     std::map<uintptr_t, ClientInfo> info;
