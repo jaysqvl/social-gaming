d80321b095749de8ccb159a1170ed84abc7acf35
mjm26 <mjm26@sfu.ca>
2023-10-17 18:17:00 -0700

    added some more functionality to syntax

--- lib/syntax/include/SyntaxGenerator.hpp
+++ lib/syntax/include/SyntaxGenerator.hpp
@@ -5,7 +5,16 @@
 
 class SyntaxGenerator {
 public:
-    SyntaxGenerator(void);
+    SyntaxGenerator(const std::string &source);
     std::unique_ptr<SyntaxNode> generate(
-            const std::string & name, const ts::Node &node);
+            const std::string &name, const ts::Node &node);
+private:
+    const std::string &source;
+private:
+    std::unique_ptr<SyntaxNode> generateConfiguration(
+            const ts::Node &node);
+    std::unique_ptr<SyntaxNode> generateField(
+            std::string_view name, const ts::Node &node);
+    std::unique_ptr<SyntaxNode> generateAtom(
+            const ts::Node &node);
 };
