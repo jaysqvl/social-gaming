66eb15d4993551af79e5e35dda804d44ad3faf12
jaysqvl <jaysqvl@gmail.com>
2023-10-17 22:29:43 -0700

    Edited Test CMakes

--- tests/lib/UserTests.cpp
+++ tests/lib/UserTests.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
+#include <limits>
 
-#include "User.hpp"  // Your module header
+#include "User.hpp"
 
 using namespace testing;
 
@@ -11,16 +12,23 @@ TEST(UserTests, getName()) {
     User user(name, id);
 
     ASSERT_EQ(name, user.getName());
-    // ASSERT_EQ(1, function1());  // Replace with actual tests
 }
 
 TEST(UserTests, getId()) {
-    std::string name = "Bob";
-    uint32_t id = 1;
-    User user(name, id);
+    std::string name1 = "Bob";
+    uint32_t id1 = 1;
+    User user1(name1, id1);
+    ASSERT_EQ(id1, user1.getId());
 
-    ASSERT_EQ(id, user.getId());
-    // ASSERT_EQ(1, function1());  // Replace with actual tests
+    std::string name2 = "David";
+    uint32_t id2 = std::numeric_limits<uint32_t>::max();
+    User user2(name2, id2);
+    ASSERT_EQ(id2, user2.getId());
+
+    std::string name3 = "Barbara";
+    uint32_t id3 = std::numeric_limits<uint32_t>::min();
+    User user3(name3, id3);
+    ASSERT_EQ(id3, user3.getId());
 }
 
 TEST(UserTests, getMap()) {
@@ -28,8 +36,7 @@ TEST(UserTests, getMap()) {
     uint32_t id = 1;
     User user(name, id);
 
-    ASSERT_EQ(0, user.getMap());
-    // ASSERT_EQ(1, function1());  // Replace with actual tests
+    ASSERT_EQ(NULL, user.getMap());
 }
 
 TEST(UserTests, getRole()) {
@@ -37,7 +44,14 @@ TEST(UserTests, getRole()) {
     uint32_t id = 1;
     User user(name, id);
 
+    ASSERT_EQ(NULL, user.getRole());
+
     user.setRole(UserRole::Owner);
-    ASSERT_EQ(1, user.getRole());
-    // ASSERT_EQ(1, function1());  // Replace with actual tests
+    ASSERT_EQ(UserRole::Owner, user.getRole());
+
+    user.setRole(UserRole::Player);
+    ASSERT_EQ(UserRole::Player, user.getRole());
+
+    user.setRole(UserRole::Audience);
+    ASSERT_EQ(UserRole::Audience, user.getRole());
 }
