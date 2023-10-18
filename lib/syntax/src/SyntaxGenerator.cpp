66457fb7f7b161558ccaa339a6cecd48adfa2701
mjm26 <mjm26@sfu.ca>
2023-10-17 18:17:00 -0700

    added some more functionality to syntax

--- lib/syntax/src/SyntaxGenerator.cpp
+++ lib/syntax/src/SyntaxGenerator.cpp
@@ -1,10 +1,76 @@
 #include "SyntaxGenerator.hpp"
 
+#include <iostream>
 
-SyntaxGenerator::SyntaxGenerator(void) {
+SyntaxGenerator::SyntaxGenerator(const std::string &source) :
+    source{source} {}
 
-}
 std::unique_ptr<SyntaxNode> SyntaxGenerator::generate(
         const std::string & name, const ts::Node &node) {
-    return std::make_unique<SyntaxNode>("program", name, 0);
+    auto result = std::make_unique<SyntaxNode>("program", name, 0);
+
+    size_t numChildren = node.getNumChildren();
+    for (size_t i = 0; i < numChildren; i++) {
+        auto child = node.getChild(i);
+        if (child.getType() == "configuration") {
+            result->append(generateConfiguration(child));
+        }
+    }
+
+    return result;
+}
+
+std::unique_ptr<SyntaxNode> SyntaxGenerator::generateConfiguration(
+        const ts::Node &node) {
+    auto result = std::make_unique<SyntaxNode>("configuration", "", 0);
+
+    size_t numChildren = node.getNumChildren();
+    for (size_t i = 0; i < numChildren; i++) {
+        auto child = node.getChild(i);
+        auto type = child.getType();
+        if (type[type.size() - 1] == ':') {
+            auto next = node.getChild(++i);
+            if (next.getType() == "{") {
+                result->append(generateField(type, node.getChild(++i)));
+            } else {
+                result->append(generateField(type, next));
+            }
+        } else {
+            // std::cout << child.getType() << std::endl;
+        }
+    }
+
+    return result;
+}
+
+std::unique_ptr<SyntaxNode> SyntaxGenerator::generateField(
+        std::string_view name, const ts::Node &node) {
+    auto result = std::make_unique<SyntaxNode>("field", std::string(name), 0);
+
+    size_t numChildren = node.getNumChildren();
+    for (size_t i = 0; i < numChildren; i++) {
+        auto child = node.getChild(i);
+        auto type = child.getType();
+
+        if (type == "(") {
+            auto temp = std::make_unique<SyntaxNode>("range", "", 0);
+            do {
+                temp->append(generateAtom(node.getChild(++i)));
+            } while (node.getChild(++i).getType() == ",");
+            result->append(std::move(temp));
+        } else if (type == "false" || type == "true") {
+            result->append(generateAtom(child));
+        } else {
+            std::cout << child.getType() << std::endl;
+        }
+    }
+
+    return result;
+}
+std::unique_ptr<SyntaxNode> SyntaxGenerator::generateAtom(
+        const ts::Node &node) {
+    return std::make_unique<SyntaxNode>(
+            std::string(node.getType()),
+            std::string(node.getSourceRange(source)),
+            0);
 }
