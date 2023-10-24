1fbe207c5619783eade4ad6f2f7a44bfd3d87365
mjm26 <mjm26@sfu.ca>
2023-10-23 17:57:30 -0700

    added basic code for variant

--- tools/tree-sitter-demo/demo.cpp
+++ tools/tree-sitter-demo/demo.cpp
@@ -4,10 +4,146 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <memory>
 
 #include <cpp-tree-sitter.h>
 #include "SyntaxNode.hpp"
 #include "SyntaxGenerator.hpp"
+
+// Node Visitor
+#include <variant>
+
+namespace Visitor {
+
+struct None;
+struct String;
+struct Pair;
+struct List;
+struct Dictionary;
+using Data = std::variant<
+    None, String,
+    Pair, List, Dictionary
+>;
+
+struct None {};
+
+struct String {
+    std::string value;
+};
+
+struct Pair {
+    std::shared_ptr<Data> first;
+    std::shared_ptr<Data> second;
+
+    Pair(const Data &first, const Data &second) :
+        first{std::make_shared<Data>(first)},
+        second(std::make_shared<Data>(second)) {}
+};
+
+struct List {
+    std::vector<Data> value;
+};
+
+struct Dictionary {
+    std::map<std::string, Data> value;
+};
+
+class TreeVisitor {
+public:
+    TreeVisitor(const std::string &source) :
+        source{source} {}
+    Data Visit(const ts::Node &node) {
+        auto type = node.getType();
+        if (type == "game") {
+            auto result = Dictionary{};
+            size_t numChildren = node.getNumChildren();
+            for (size_t i = 0; i < numChildren; i++) {
+                auto child = node.getChild(i);
+                auto temp = Visit(child);
+
+                if (std::holds_alternative<Pair>(temp)) {
+                    auto pair = std::get<Pair>(temp);
+                    if (std::holds_alternative<String>(*pair.first)) {
+                        auto key = std::get<String>(*pair.first);
+                        result.value[key.value] = *pair.second;
+                    }
+                }
+            }
+            return result;
+        } else if (type == "configuration" /*||
+                type == "constants" ||
+                type == "variables" ||
+                type == "per_player" ||
+                type == "per_audience" ||
+                type == "rules" */) {
+            if (node.getNumChildren() > 1) {
+                auto key = std::string(node.getChild(0).
+                        getSourceRange(source));
+                auto value = VisitSibling(node.getChild(1));
+                return Pair(String{key}, value);
+            } else {
+                return None{};
+            }
+        } else if (type == "comment") {
+            return None{};
+        } else {
+            std::cout << "Visit " << node.getType() << std::endl;
+            return None{};
+        }
+    }
+    Data VisitSibling(const ts::Node &node) {
+        auto type = node.getType();
+        if (type == "{") {
+            Dictionary result;
+            ts::Node next = node.getNextSibling();
+            while (next.getType() != "}") {
+                auto key = std::string(next.getSourceRange(source));
+                auto child = next.getNextSibling();
+                auto temp = VisitSibling(child);
+                next = child.getNextSibling();
+            }
+            return result;
+        } else {
+            std::cout << "Sibling " << node.getType() << std::endl;
+            return None{};
+        }
+    }
+private:
+    std::string source;
+};
+
+struct Printer {
+    void operator()(const None &none) {
+        std::cout << "None" << std::endl;
+    }
+    void operator()(const String &string) {
+        std::cout << "String " << string.value << std::endl;
+    }
+    void operator()(const Pair &pair) {
+        /*std::cout << "Pair" << std::endl;
+        std::cout << "  ";
+        std::visit(*this, pair.first);
+        std::cout << "  ";
+        std::visit(*this, pair.second);*/
+    }
+    void operator()(const List &list) {
+        /*std::cout << "List" << std::endl;
+        for (const auto &elem : dict.value) {
+            std::cout << "  ";
+            std::visit(*this, elem);
+        }*/
+    }
+    void operator()(const Dictionary & dict) {
+        std::cout << "Dictionary" << std::endl;
+        for (const auto &elem : dict.value) {
+            std::cout << "  " << elem.first << ": ";
+            std::visit(*this, elem.second);
+        }
+    }
+};
+
+};
 
 // Declare an external C function that provides the Tree-sitter language
 extern "C" {
@@ -54,9 +190,15 @@ int main(int argc, char *argv[]) {
   // auto treestring = root.getSExpr();
   // printf("Syntax tree: %s\n", treestring.get());
 
-  SyntaxGenerator gen{sourcecode};
-  auto syntax = gen.generate(filename, root);
-  syntax->print(0);
+  // SyntaxGenerator gen{sourcecode};
+  // auto syntax = gen.generate(filename, root);
+  // syntax->print(0);
+
+  Visitor::TreeVisitor treeVisitor{sourcecode};
+  Visitor::Data result = treeVisitor.Visit(root);
+
+  std::cout << "== Result ==" << std::endl;
+  std::visit(Visitor::Printer{}, result);
 
   // Return 0 to indicate successful execution
   return 0;
