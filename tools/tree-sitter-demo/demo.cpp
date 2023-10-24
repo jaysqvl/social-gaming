d7b49e9875a836bf2756c328f35df517145b596e
mjm26 <mjm26@sfu.ca>
2023-10-23 22:03:16 -0700

    got variant parser working for constants

--- tools/tree-sitter-demo/demo.cpp
+++ tools/tree-sitter-demo/demo.cpp
@@ -17,19 +17,41 @@
 namespace Visitor {
 
 struct None;
+struct Integer;
+struct Boolean;
 struct String;
+struct Identifier;
+struct Range;
 struct Pair;
 struct List;
 struct Dictionary;
 using Data = std::variant<
-    None, String,
+    None, Integer, Boolean,
+    String, Identifier, Range,
     Pair, List, Dictionary
 >;
 
 struct None {};
 
+struct Integer {
+    int value;
+};
+
+struct Boolean {
+    bool value;
+};
+
 struct String {
     std::string value;
+};
+
+struct Identifier {
+    std::string value;
+};
+
+struct Range {
+    int begin;
+    int end;
 };
 
 struct Pair {
@@ -71,41 +93,100 @@ public:
                 }
             }
             return result;
-        } else if (type == "configuration" /*||
-                type == "constants" ||
+        } else if (type == "configuration" ||
+                type == "constants" /*||
                 type == "variables" ||
                 type == "per_player" ||
                 type == "per_audience" ||
-                type == "rules" */) {
+                type == "rules" */ ||
+                type == "setup_rule") {
             if (node.getNumChildren() > 1) {
                 auto key = std::string(node.getChild(0).
                         getSourceRange(source));
-                auto value = VisitSibling(node.getChild(1));
+                ts::Node child = node.getChild(1);
+                auto value = VisitSibling(child);
                 return Pair(String{key}, value);
             } else {
                 return None{};
             }
+        } else if (node.getType() == "value_map") {
+            auto child = node.getChild(0);
+            return VisitSibling(child);
+        } else if (type == "map_entry") {
+            auto key = std::string(node.getChild(0).
+                    getSourceRange(source));
+            auto value = Visit(node.getChild(2));
+            return Pair{String{key}, value};
+        } else if (type == "expression") {
+            return Visit(node.getChild(0));
+        } else if (type == "list_literal") {
+            return Visit(node.getChild(1));
+        } else if (type == "expression_list") {
+            List result;
+            for (size_t i = 0; i < node.getNumChildren(); i += 2) {
+                result.value.push_back(Visit(node.getChild(i)));
+            }
+            return result;
+        } else if (node.getType() == "quoted_string") {
+            auto temp = std::string(node.getSourceRange(source));
+            return String{temp};
+        } else if (node.getType() == "number_range") {
+            auto begin = std::string(node.getChild(1).
+                    getSourceRange(source));
+            auto end = std::string(node.getChild(3).
+                    getSourceRange(source));
+            return Range{std::stoi(begin), std::stoi(end)};
+        } else if (node.getType() == "boolean") {
+            auto temp = node.getChild(0).getSourceRange(source);
+            return Boolean{temp[0] == 't'};
+        } else if (node.getType() == "integer") {
+            auto temp = std::string(node.getSourceRange(source));
+            return Identifier{temp};
         } else if (type == "comment") {
             return None{};
         } else {
             std::cout << "Visit " << node.getType() << std::endl;
+            for (size_t i = 0; i < node.getNumChildren(); i++) {
+                std::cout << "  " << node.getChild(i).getType() << std::endl;
+            }
             return None{};
         }
     }
-    Data VisitSibling(const ts::Node &node) {
-        auto type = node.getType();
-        if (type == "{") {
+
+    Data VisitSibling(ts::Node &node) {
+        if (node.getType() == "{") {
             Dictionary result;
-            ts::Node next = node.getNextSibling();
-            while (next.getType() != "}") {
-                auto key = std::string(next.getSourceRange(source));
-                auto child = next.getNextSibling();
-                auto temp = VisitSibling(child);
-                next = child.getNextSibling();
+            node = node.getNextSibling();
+            while (node.getType() != "}") {
+                auto temp = VisitSibling(node);
+
+                if (std::holds_alternative<Pair>(temp)) {
+                    auto pair = std::get<Pair>(temp);
+                    if (std::holds_alternative<String>(*pair.first)) {
+                        auto key = std::get<String>(*pair.first);
+                        result.value[key.value] = *pair.second;
+                    }
+                }
             }
             return result;
+        } else if (node.getType()[node.getType().size() - 1] == ':') {
+            auto key = std::string(node.getType().substr(0, node.getType().size() - 1));
+            node = node.getNextSibling();
+            auto temp = VisitSibling(node);
+            return Pair{String{key}, temp};
+        } else if (node.getType() == "setup_rule" ||
+                node.getType() == "value_map" ||
+                node.getType() == "map_entry" ||
+                node.getType() == "quoted_string" ||
+                node.getType() == "number_range" ||
+                node.getType() == "boolean" ||
+                node.getType() == "integer") {
+            auto temp = Visit(node);
+            node = node.getNextSibling();
+            return temp;
         } else {
             std::cout << "Sibling " << node.getType() << std::endl;
+            node = node.getNextSibling();
             return None{};
         }
     }
@@ -117,22 +198,36 @@ struct Printer {
     void operator()(const None &none) {
         std::cout << "None" << std::endl;
     }
+    void operator()(const Integer &integer) {
+        std::cout << "Integer " << integer.value << std::endl;
+    }
+    void operator()(const Boolean &boolean) {
+        std::cout << "Boolean " << (boolean.value ? "true" : "false")
+            << std::endl;
+    }
     void operator()(const String &string) {
         std::cout << "String " << string.value << std::endl;
     }
+    void operator()(const Identifier &identifier) {
+        std::cout << "Identifier " << identifier.value << std::endl;
+    }
+    void operator()(const Range &range) {
+        std::cout << "Range (" << range.begin << ", "
+            << range.end << ")" << std::endl;
+    }
     void operator()(const Pair &pair) {
-        /*std::cout << "Pair" << std::endl;
+        std::cout << "Pair" << std::endl;
         std::cout << "  ";
-        std::visit(*this, pair.first);
+        std::visit(*this, *pair.first);
         std::cout << "  ";
-        std::visit(*this, pair.second);*/
+        std::visit(*this, *pair.second);
     }
     void operator()(const List &list) {
-        /*std::cout << "List" << std::endl;
-        for (const auto &elem : dict.value) {
+        std::cout << "List" << std::endl;
+        for (const auto &elem : list.value) {
             std::cout << "  ";
             std::visit(*this, elem);
-        }*/
+        }
     }
     void operator()(const Dictionary & dict) {
         std::cout << "Dictionary" << std::endl;
