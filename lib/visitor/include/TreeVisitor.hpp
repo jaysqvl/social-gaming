ab2e772464042f15537d1bb2fc72fc0b6b102fde
drlowe <david_lowe@sfu.ca>
2023-11-03 22:57:57 -0700

    cleaned up visit function

--- lib/visitor/include/TreeVisitor.hpp
+++ lib/visitor/include/TreeVisitor.hpp
@@ -10,9 +10,22 @@ class TreeVisitor {
 public:
     TreeVisitor(const std::string &source);
     Data Visit(const ts::Node &node);
+    Data VisitGame(const ts::Node &node);
+    Data VisitPair(const ts::Node &node);
+    Data VisitFirstChild(const ts::Node &node);
+    Data VisitFirstSibling(const ts::Node &node);
+    Data VisitMapEntry(const ts::Node &node);
+    Data VisitListLiteral(const ts::Node &node);
+    Data VisitExpressionList(const ts::Node &node);
+    Data VisitQuotedString(const ts::Node &node);
+    Data VisitNumberRange(const ts::Node &node);
+    Data VisitBoolean(const ts::Node &node);
+    Data VisitNumber(const ts::Node &node);
+    Data VisitInteger(const ts::Node &node);
     Data VisitSibling(ts::Node &node);
 private:
     std::string source;
+   
 };
 
 };
