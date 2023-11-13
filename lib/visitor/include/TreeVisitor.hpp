c8f7ce9a01a918cbcc96a5b2791f4ea74839b6a6
amosk <amosk@sfu.ca>
2023-11-13 23:22:30 +0000

    Revert "Try to parse rules"
    This reverts commit 6579cb04f9868da2efb575c1ea15bfd1541486e3

--- lib/visitor/include/TreeVisitor.hpp
+++ lib/visitor/include/TreeVisitor.hpp
@@ -11,13 +11,6 @@ public:
     TreeVisitor(const std::string &source);
     Data Visit(const ts::Node &node);
     Data VisitSibling(ts::Node &node);
-    Data ParseRules(const ts::Node &node);
-    Data ParseLoopBody(const ts::Node &node);
-    Data ParseMatchCondition(const ts::Node &node);
-    Data ParseTrueBranch(const ts::Node &node);
-    Data ParseDiscardStatement(const ts::Node &node);
-
-
 private:
     std::string source;
 };
