c833c291986b0b24e56d78293de4db610038bd78
tzaloilo <tzaloilo@sfu.ca>
2023-11-21 23:35:03 -0800

    Starting improvements to GameManager, GeneralManager, Game, User in order to get I/O to work

--- lib/manager/include/GameManager.hpp
+++ lib/manager/include/GameManager.hpp
@@ -8,6 +8,8 @@
 //#include "Rule.hpp"
 //#include "Element.hpp"
 
+#include "GeneralManager.hpp"
+
 #include "Game.hpp"
 
 #include <vector>
@@ -19,26 +21,26 @@ using networking::Connection;
 
 // class Rule;
 
+// Resolve circular dependency
+class GeneralManager;
+
 class GameManager {
 private:
     //using RulePtr = std::shared_ptr<Rule>;
 
-    // TODO - add a vector of User
-    // User = {connection, room}
-    // TODO - add single reference to Game
-    // TODO - remove the below
-    
+    // using this, you can send commands back to the parent general manager.
+    GeneralManager* generalManager;
 
     std::vector<Connection> clients;
-
+    // Game game;
+    std::string_view gameName;
+    bool audience;
+    std::pair<int, int> playerRange;
 
 
     // User owner;
-    // std::string gameName;
-    // std::pair<int, int> playerRange;
-    // bool audience;
     // // Stores the setups the game has the parse and execute
-    // std::vector<Setup> setups;
+    std::vector<Setup> setups;
 
     // //TODO: FIX CIRCULAR DEPENDENCY
     // std::vector<RulePtr> rules; // game manager should have rule, but rule should not have gamemanager
@@ -47,17 +49,26 @@ private:
     // std::map<std::string, ElementPtr> variables;
 
     // //A map holding key value pairs to store a unique user's name, as well as the data for that user
-    // std::shared_ptr<std::map<std::string, User>> playerMap = std::make_shared<std::map<std::string, User>>();
-    // std::shared_ptr<std::map<std::string, User>> spectatorMap = std::make_shared<std::map<std::string, User>>();
+
+    // TODO - fix / change this.
+    std::shared_ptr<std::map<std::string, User>> playerMap = std::make_shared<std::map<std::string, User>>();
+    std::shared_ptr<std::map<std::string, User>> spectatorMap = std::make_shared<std::map<std::string, User>>();
 
 public:
-    GameManager(std::string name, User owner);
+    
+    // TODO - change back to users maybe?
+
+    GameManager(std::string_view name, Connection& conn, GeneralManager* generalManager);
+    // GameManager(std::string name, User owner, std::vector<Connection> clients);
     bool addPlayer(std::string name);
     bool removePlayer(std::string name);
     bool addSpectator(std::string name);
     bool removeSpectator(std::string name);
     bool hasPlayer(std::string name);
     bool hasAudience();
+
+    // function that sends message to a list of players in the game
+
 
     std::vector<Setup> getSetups();
     // std::map<std::string, ElementPtr> getConstants();
