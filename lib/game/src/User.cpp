ec64c903bf2849a3cb8435ab7bbe26f7de38d51c
tzaloilo <tzaloilo@sfu.ca>
2023-11-30 01:09:37 -0800

    A ton (TON) of refactoring. Changed the way packets build and send. Added functionality to send system messages from game lobbies.

--- lib/game/src/User.cpp
+++ lib/game/src/User.cpp
@@ -3,16 +3,20 @@
 
 // User::User(std::string name, uint32_t id) : name(name), id(id){};
 
-User::User(std::string name) : name(name) {};
+// TODO - fix back to normal ID
+User::User(uint32_t id) : id(id), name("User " + std::to_string(id)) {};
 
 std::string User::getName() const {
     return name;
 }
 
+bool User::operator<(const User& other) const {
+    return id < other.getId();
+}
 
-// uint32_t User::getId() const {
-//     return id;
-// }
+uint32_t User::getId() const {
+    return id;
+}
 
 // std::map<std::string, ElementPtr> User::getMap() const {
 //     return userMap;
--- lib/manager/src/GameManager.cpp
+++ lib/manager/src/GameManager.cpp
@@ -20,46 +20,53 @@ void loadFile(std::string &contents, const std::string &filename) {
     );
 }
 
-// Constructor for the GameManager class
-GameManager::GameManager(std::string_view name, Connection& conn, GeneralManager* generalManager) :
-    generalManager(generalManager),
-    gameName(name),
-    audience(false),
-    playerRange(std::make_pair(2, 8)) { //temp playerRange value
-    // Initialize the GameManager with the owner and game name
-    // Set the initial conditions
-    // Adjust playerRange as needed
-
-    // the player's default username is "User <connectionID>"
-    auto username = "User " + conn.id;
-    addPlayer(username, conn);
+bool ends_with(const std::string_view &str, const std::string_view &end) {
+    if (end.size() > str.size()) {
+        return false;
+    } else {
+        return str.substr(str.size() - end.size(), end.size()) == end;
+    }
 }
 
-// Add a player with the given name
-// bool GameManager::addPlayer(std::string name) {
-//     if (playerMap->size() >= playerRange.first && playerMap->size() < playerRange.second) {
-//         // Check if the number of players is within the allowed range
-//         auto result = playerMap->emplace(name, User(name, playerMap->size() + 1));
-//         return result.second; // Return true if the insertion was successful
-//     }
-//     return false; // Return false if the player limit has been reached
-// }
+// Constructor for the GameManager class
+GameManager::GameManager(std::string_view name, Connection& conn) :
+    gameName(name),
+    audience(false),
+    playerRange(std::make_pair(2, 8)) { // Default playerRange value
+    // The player's default username is "User <connectionID>"
+    addPlayer(conn);
+}
 
-bool GameManager::addPlayer(std::string name, Connection& conn) {
 
-    if (playerMap->size() >= playerRange.first && playerMap->size() < playerRange.second) {
+bool GameManager::addPlayer(Connection& conn) {
+
+    // make sure that the player limit has not been reached
+    if (playerMap->size() + 1 < playerRange.second) {
+
         // Check if the number of players is within the allowed range
+        std::cout << std::to_string(conn.id) + " added to game " + getGameName() << std::endl;
+
+        auto newUser = User(conn.id);
+
+        // if this is the only player in the current game, they become the owner.
+        // if (playerMap->size() == 0){
+        //     owner = newUser;
+        // }
 
         // add a mapping of a new User and their Connection to GameManager.
-        auto result = playerMap->emplace(User(name), conn);
+        auto result = playerMap->emplace(newUser, conn);
+
+        // let everyone in the server know that a new player has joined
+        sendSystemMessage("Welcome player " + newUser.getName() + " to the game lobby " + getGameName());
+
         return result.second; // Return true if the insertion was successful
     }
+    
     return false; // Return false if the player limit has been reached
 }
 
 // Remove a player with the given name. Returns true if removed successfully
 bool GameManager::removePlayer(std::string name) {
-    
     auto it = std::find_if(playerMap.get()->begin(), playerMap.get()->end(),
                            [name](const std::pair<const User, Connection>& pair) {
                                return pair.first.getName() == name;
@@ -75,7 +82,6 @@ bool GameManager::removePlayer(std::string name) {
 }
 
 // Check if a player with the given name exists
-// TODO - use this same function in removePlayer()
 bool GameManager::hasPlayer(std::string name) {
     
     auto it = std::find_if(playerMap.get()->begin(), playerMap.get()->end(),
@@ -91,6 +97,7 @@ bool GameManager::hasPlayer(std::string name) {
     return false;
 }
 
+// check if a given connection exists in this game
 bool GameManager::hasConnection(const Connection& conn) {
     
     auto it = std::find_if(playerMap.get()->begin(), playerMap.get()->end(),
@@ -106,17 +113,46 @@ bool GameManager::hasConnection(const Connection& conn) {
     return false;
 }
 
-// returns all the connections in the game
+std::string GameManager::getUsername(const Connection& conn){
+
+    auto it = std::find_if(playerMap.get()->begin(), playerMap.get()->end(),
+                           [conn](const std::pair<const User, Connection>& pair) {
+                               return pair.second == conn;
+                           });
+
+    // Check if the User object was found
+    if (it != playerMap->end()) {
+        // Remove the User-Connection pair from the map
+        return it->first.getName();
+    } 
+    return "Null";
+}
+
+// returns all the connections in the current game
 std::vector<Connection> GameManager::getConnections() {
     std::vector<Connection> connections;
+    const std::map<User, Connection>& playerMapLocal = *playerMap;
 
-    for (const auto& conn : playerMap){
-        connections.push_back(conn.second);
+    for (auto it = playerMapLocal.begin(); it != playerMapLocal.end(); ++it) {
+        const Connection& conn = it->second;
+        connections.push_back(conn);
+        // Process user and connection
     }
     return connections;
 }
 
-std::string_view GameManager::getGameName() const {
+// returns all connections and their usernames in the current game
+std::vector<std::pair<Connection, std::string>> GameManager::getConnectionsUsernames(){
+    std::vector<std::pair<Connection, std::string>> connections;
+    const std::map<User, Connection>& playerMapLocal = *playerMap;
+
+    for (auto it = playerMapLocal.begin(); it != playerMapLocal.end(); ++it) {
+        connections.push_back(std::make_pair(it->second, it->first.getName()));
+    }
+    return connections;
+}
+
+std::string GameManager::getGameName() const {
     return gameName;
 };
 
@@ -124,21 +160,20 @@ std::vector<Setup> GameManager::getSetups() {
     return setups;
 }
 
-// std::map<std::string, ElementPtr> GameManager::getConstants() {
-//     return constants;
-// }
-
-// std::map<std::string, ElementPtr> GameManager::getVariables() {
-//     return variables;
-// }
-
-bool ends_with(const std::string_view &str, const std::string_view &end) {
-    if (end.size() > str.size()) {
-        return false;
-    } else {
-        return str.substr(str.size() - end.size(), end.size()) == end;
+void GameManager::sendSystemMessage(std::string message){
+    // create a packet to send to each connection in the game.   
+    for (const Connection& conn : getConnections()){
+            systemMessages.push_back(Packet{PacketType::TO, conn, message});
     }
 }
+
+std::vector<Packet> GameManager::retrieveSystemMessages(){
+    std::vector<Packet> systemPackets(systemMessages);
+    systemMessages.clear();
+    return systemPackets;
+
+}
+
 
 void GameManager::parseAndSetRules() {
 
